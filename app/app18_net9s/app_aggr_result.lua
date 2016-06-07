d3a5402c7def7a0f745b8168a3364f6c686d2dd6
Ameen Eetemadi <ameenetemady@github.com>
2016-06-07 14:41:15 -0400

    update app_aggr_result to include all done experiments

--- app/app18_net9s/app_aggr_result.lua
+++ app/app18_net9s/app_aggr_result.lua
@@ -5,21 +5,50 @@ local taSet = {
 ----[[
   { strFormat= "result/grnn_d_%s.table",
     taFields = { "dTrainErr", "taTestResult"},
-    nMaxId = 10 },
+    nMaxId = 100 },
   { strFormat= "result/grnn_d_%s_noise.table",
     taFields = { "dTrainErr", "taTestResult"},
-    nMaxId = 10 },
+    nMaxId = 100 },
   { strFormat= "result/fnn_nh0_nnpl4_d_%s.table",
     taFields = { "dTrainErr", "taTestResult"},
-    nMaxId = 10 },
+    nMaxId = 100 },
+  { strFormat= "result/fnn_nh1_nnpl4_d_%s.table",
+    taFields = { "dTrainErr", "taTestResult"},
+    nMaxId = 100 },
+  { strFormat= "result/fnn_nh2_nnpl4_d_%s.table",
+    taFields = { "dTrainErr", "taTestResult"},
+    nMaxId = 100 },
+  { strFormat= "result/fnn_nh3_nnpl4_d_%s.table",
+    taFields = { "dTrainErr", "taTestResult"},
+    nMaxId = 100 },
+  { strFormat= "result/fnn_nh4_nnpl4_d_%s.table",
+    taFields = { "dTrainErr", "taTestResult"},
+    nMaxId = 100 },
   { strFormat= "result/fnn_nh0_nnpl4_d_%s_noise.table",
     taFields = { "dTrainErr", "taTestResult"},
-    nMaxId = 10 }
+    nMaxId = 100 },
+  { strFormat= "result/fnn_nh1_nnpl4_d_%s_noise.table",
+    taFields = { "dTrainErr", "taTestResult"},
+    nMaxId = 100 },
+  { strFormat= "result/fnn_nh2_nnpl4_d_%s_noise.table",
+    taFields = { "dTrainErr", "taTestResult"},
+    nMaxId = 100 },
+  { strFormat= "result/fnn_nh3_nnpl4_d_%s_noise.table",
+    taFields = { "dTrainErr", "taTestResult"},
+    nMaxId = 100 },
+  { strFormat= "result/fnn_nh4_nnpl4_d_%s_noise.table",
+    taFields = { "dTrainErr", "taTestResult"},
+    nMaxId = 100 }
+
 
 }
 
 for k, v in pairs(taSet) do
   local taSummary = aggr_result.getAggrSummary(v)
-  print("*****" .. string.format(v.strFormat, "*") .. ":")
-  print(taSummary)
+  
+  print( 
+        string.format("%.4f", taSummary.taTestResult.mean) .. "±" ..
+        string.format("%.4f", taSummary.taTestResult.std) .. "|" ..
+        string.format(v.strFormat, "*"))
+--  print(taSummary)
 end
